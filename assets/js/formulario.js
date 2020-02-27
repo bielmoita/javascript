311f2c4d7cace20720d9a6da737ce53c77c78ac6
Gabriel Fernando <oficial.gabriel89@gmail.com>
2020-02-27 16:01:45 -0300

    Arrumando data para formato PT-BR

--- assets/js/formulario.js
+++ assets/js/formulario.js
@@ -3,14 +3,22 @@ function mostrar()
     var nome = document.getElementById("nome").value
     var idade = document.getElementById("idade").value
     var nascimento = document.getElementById("nascimento").value
+    // Arrumando a data para o PT-BR, Data aparece: 
+    // 2020/02/27 - Ele gera como String
+    // 0123456789 --> posição da String Data
+    var dia = nascimento.substring(8,10)
+    var mes = nascimento.substring(5,7)
+    var ano = nascimento.substring(0,4)
+    nascimento = dia+"/"+mes+"/"+ano
+
     var observacao = document.getElementById("observacao").value
-    var estado = document.getElementById("estados").value
+    var estados = document.getElementById("estados").value
     var senha = document.getElementById("senha").value
     var genero = document.querySelector('input[name="sexo"]:checked').value
     // var opcao = document.getElementById("almocoOuTransporte").checkbox
     
-    var almoco=document.getElementById("almoco").value
-    if (almoco.checked)
+    var almoco=document.getElementById("almoco").checked
+    if (almoco==true)
     {   
         almoco = "sim"
     }
@@ -18,8 +26,8 @@ function mostrar()
         almoco="não"
     }
 
-    var transporte=document.getElementById("transporte").value
-    if (transporte.checked)
+    var transporte=document.getElementById("transporte").checked
+    if (transporte==true)
     {   
         transporte = "sim"
     }
@@ -29,6 +37,6 @@ function mostrar()
 
 
 
-    alert ("Nome: "+nome+ "\nIdade: "+idade +"\nNascimento: "+nascimento+ "\n Observacao: "+observacao+ "\nEstado: "+estados+ "\n Senha: "+senha+ "\nGenero: "+genero+ "\nOpções: "+almoco+ "\nOpções: "+transporte)
+    alert ("Nome: "+nome+ "\nIdade: "+idade +"\nNascimento: "+nascimento+ "\n Observacao: "+observacao+ "\nEstado: "+estados+ "\n Senha: "+senha+ "\nGenero: "+genero+ "\nOpção de Almoço: "+almoco+ "\nOpção de Vale transporte: "+transporte)
 
 }
